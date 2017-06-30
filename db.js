10999f577b79b28113db11a550579056cfab25fb
duongoku <nguyenvubinhduong@gmail.com>
2017-06-30 09:26:53 +0700

    Commit #2

--- db.js
+++ db.js
@@ -6,16 +6,16 @@ const db = new nedb({
 });
 
 const defaultData = [
-	{ _id: 1, main: "Can the jargon score? "},
-	{ _id: 2, main: "This referendum disappears against each acorn! "},
-	{ _id: 3, main: "The episode moans above the go censorship. "},
-	{ _id: 4, main: "Her scenario fields a circular mania. "},
-	{ _id: 5, main: "The instinct trigger discriminates beside the isolate taxi. "},
-	{ _id: 6, main: "The recovered anguish begs the accountant above each bull matrix. "},
-	{ _id: 7, main: "My audience appears. "},
-	{ _id: 8, main: "An assault volunteers without the satellite. "},
-	{ _id: 9, main: "Near a researcher talks the brick. "},
-	{ _id: 10, main: "The spread clearance slides beside the institute. "}
+	{ _id: 1, main: "1.Can the jargon score? "},
+	{ _id: 2, main: "2.This referendum disappears against each acorn! "},
+	{ _id: 3, main: "3.The episode moans above the go censorship. "},
+	{ _id: 4, main: "4.Her scenario fields a circular mania. "},
+	{ _id: 5, main: "5.The instinct trigger discriminates beside the isolate taxi. "},
+	{ _id: 6, main: "6.The recovered anguish begs the accountant above each bull matrix. "},
+	{ _id: 7, main: "7.My audience appears. "},
+	{ _id: 8, main: "8.An assault volunteers without the satellite. "},
+	{ _id: 9, main: "9.Near a researcher talks the brick. "},
+	{ _id: 10, main: "10.The spread clearance slides beside the institute. "}
 ];
 
 db.count({}, (err, count) => {
